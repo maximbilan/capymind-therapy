8a622bd85cc4b4b37f6fbc11b5b779f2f3c68c70
Maksym Bilan <maximb.mail@gmail.com>
2025-10-12 22:16:52 +0300

    Deployment permissions script

---
+++ scripts/deployment_account_perms.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PROJECT_ID=$CAPY_PROJECT_ID
+SERVICE_ACCOUNT=$CAPY_SERVICE_ACCOUNT
+PROJECT_NUMBER=$CAPY_PROJECT_NUMBER
+REGION=$CAPY_SERVER_REGION
+POOL_ID="githubci"
+POOL_NAME="GitHubCI"
+LOCATION="global"
+PROVIDER_ID="github-provider"
+PROVIDER_NAME="GitHub Provider"
+REPO="maximbilan/capymind-session"
+
+PRINCIPAL_SET="principalSet://iam.googleapis.com/projects/$PROJECT_NUMBER/locations/$LOCATION/workloadIdentityPools/$POOL_ID/attribute.repository/$REPO"
+
+gcloud iam service-accounts add-iam-policy-binding $SERVICE_ACCOUNT \
+  --project=$PROJECT_ID \
+  --role="roles/iam.workloadIdentityUser" \
+  --member=$PRINCIPAL_SET
