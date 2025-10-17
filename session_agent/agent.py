19a9b5d07b95e7bd249ec5a5cdf467f1b258bcb4
Maksym Bilan <maximb.mail@gmail.com>
2025-10-17 13:43:48 +0300

    Descr

--- session_agent/agent.py
+++ session_agent/agent.py
@@ -3,6 +3,6 @@ from google.adk.agents.llm_agent import Agent
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    description='An AI agent that handles therapy session requests',
+    instruction='Provide support and guidance for mental health therapy-related inquiries',
 )
