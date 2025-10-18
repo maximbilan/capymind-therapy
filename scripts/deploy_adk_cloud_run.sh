a393ec48c1de6790fe9b2df88da6d47d77e13fb2
Maksym Bilan <maximb.mail@gmail.com>
2025-10-18 15:01:07 +0300

    service name

--- scripts/deploy_adk_cloud_run.sh
+++ scripts/deploy_adk_cloud_run.sh
@@ -19,7 +19,7 @@ SECRETS=${SECRETS%,}
 adk deploy cloud_run \
   --project=$CAPY_PROJECT_ID \
   --region=$CAPY_SERVER_REGION \
-  --service_name=capymind \
+  --service_name=capymind-agent \
   --app_name=capymind_agent \
   --with_ui \
 capymind_agent
