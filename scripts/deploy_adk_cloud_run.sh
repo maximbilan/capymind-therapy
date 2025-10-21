ac81e99e9424219948d324a37fc8142001e03062
Maksym Bilan <maximb.mail@gmail.com>
2025-10-21 19:14:56 +0300

    Clean up (#28)

--- scripts/deploy_adk_cloud_run.sh
+++ scripts/deploy_adk_cloud_run.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-# Set environment variables
-ENV_PARAMS=("CAPY_PROJECT_ID=$CAPY_PROJECT_ID" "CAPY_SERVER_REGION=$CAPY_SERVER_REGION" "CLOUD=true")
-ENV_VARS=""
-for PARAM in "${ENV_PARAMS[@]}"; do
-  ENV_VARS+="$PARAM,"
-done
-ENV_VARS=${ENV_VARS%,}
-
-# Set the secret environment variables
-SECRET_PARAMS=("GOOGLE_API_KEY=gemini_api_key")
-SECRETS=""
-for PARAM in "${SECRET_PARAMS[@]}"; do
-  SECRETS+="$PARAM:latest,"
-done
-SECRETS=${SECRETS%,}
-
+# Deploy the application
 adk deploy cloud_run \
   --project=$CAPY_PROJECT_ID \
   --region=$CAPY_SERVER_REGION \
