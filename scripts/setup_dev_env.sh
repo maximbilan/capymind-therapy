feada3b792abadfb06ef29dc1668597275f9a45c
Maksym Bilan <maximb.mail@gmail.com>
2025-10-25 18:19:32 +0300

    Comments

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+# Setup a Python virtual environment
 python3 -m venv .venv
 source .venv/bin/activate
+
+# Install required packages
 pip install google-adk
 pip install google-cloud-firestore
